f1ec4f50b32eea8c38cd06cf98146eb62f94906c
Diógenes Castro <diogenesxmarlon@gmail.com>
2026-05-21 15:58:15 -0300

    fix: use proper bin object format in npm-prep, add missing fields

--- scripts/npm-prep.js
+++ scripts/npm-prep.js
@@ -5,10 +5,15 @@ const root = process.cwd()
 const pkg = JSON.parse(readFileSync(`${root}/package.json`, "utf-8"))
 
 pkg.name = "better-expr-editor"
-pkg.bin = "./server.js"
+pkg.bin = { "better-expr-editor": "server.js" }
 pkg.private = false
-pkg.scripts = {}
-pkg.devDependencies = {}
+pkg.main = "server.js"
+pkg.scripts = { start: "node server.js" }
+pkg.keywords = []
+pkg.author = ""
+pkg.bugs = { url: "https://github.com/expr-lang/expr-lang-editor/issues" }
+pkg.homepage = "https://github.com/expr-lang/expr-lang-editor#readme"
+delete pkg.devDependencies
 delete pkg.dependencies
 
 mkdirSync(`${root}/npm-package/dist`, { recursive: true })
